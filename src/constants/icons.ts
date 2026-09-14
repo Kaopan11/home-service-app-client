3399f8c8e6de872535d08054bd844e65ef63af82
Kaopan <atkaew@gmail.com>
2026-09-14 15:43:38 +0700

    chore(icons): organize and rename SVG icon assets

---
+++ src/constants/icons.ts
@@ -0,0 +1,36 @@
+export const icons = {
+  navigation: {
+    list: '/icons/navigation/list.svg',
+    history: '/icons/navigation/history.svg',
+    account: '/icons/navigation/account.svg',
+    logout: '/icons/navigation/logout.svg',
+    menu: '/icons/navigation/menu.svg',
+  },
+  notification: {
+    outline: '/icons/notification/notification-outline.svg',
+    filled: '/icons/notification/notification-filled.svg',
+  },
+  contact: {
+    email: '/icons/contact/email.svg',
+    phone: '/icons/contact/phone.svg',
+    tag: '/icons/contact/tag.svg',
+    inbox: '/icons/contact/inbox.svg',
+  },
+  action: {
+    qrCode: '/icons/action/qr-code.svg',
+    editFilled: '/icons/action/edit-filled.svg',
+    editOutline: '/icons/action/edit-outline.svg',
+    trashOutline: '/icons/action/trash-outline.svg',
+    checkCircleFilled: '/icons/action/check-circle-filled.svg',
+    document: '/icons/action/document.svg',
+    mailCheck: '/icons/action/mail-check.svg',
+    imageAdd: '/icons/action/image-add.svg',
+  },
+  feedback: {
+    alertCircle: '/icons/feedback/alert-circle.svg',
+  },
+  loading: {
+    primary: '/icons/loading/spinner-primary.svg',
+    secondary: '/icons/loading/spinner-secondary.svg',
+  },
+} as const
